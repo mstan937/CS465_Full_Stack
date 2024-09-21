e668add04852de311560ff95fffcedf6e11ae3e1
Marcus Stanley <redeyem3@gmail.com>
2024-09-21 18:11:53 -0400

    Module 3 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,3 +1,7 @@
+var fs = require('fs'); 
+var trips = JSON.parse(fs.readFileSync('.data/trips.json','utf8')); 
+
+
 /* GET travel view page */
 const travel = function(req, res) {
   res.render('travel', { title: 'Travlr Getaways' });
